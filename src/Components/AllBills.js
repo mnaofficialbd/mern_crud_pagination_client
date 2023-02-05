e3a44eaf097c1e5130365e28045d44b9e2999319
mnaofficial <mna.official92@gmail.com>
2023-02-05 20:55:36 +0600

    all-bills component create

---
+++ src/Components/AllBills.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllBills = () => {
+    return (
+        <div>
+            <h2 className="text-center">All Bills</h2>
+        </div>
+    );
+};
+
+export default AllBills;
